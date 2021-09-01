9f8f7b80e664991af66934bbaf559979a6c59447
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-09-01 03:49:24 +0300

    Base command Interface and Implementation

---
+++ CndBot/Core/Commands/BaseCommand.cs
@@ -0,0 +1,10 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace CndBot.Core
+{
+    public abstract class BaseCommand : IBotCommand
+    {
+        public abstract void ExecuteCommand(ITelegramBotClient botClient, Update update);
+    }
+}
---
+++ CndBot/Core/Commands/IBotCommand.cs
@@ -0,0 +1,10 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace CndBot.Core
+{
+    public interface IBotCommand
+    { 
+        void ExecuteCommand(ITelegramBotClient botClient, Update update);
+    }
+}
