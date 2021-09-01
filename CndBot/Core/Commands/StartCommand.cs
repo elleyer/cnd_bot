03f03c69bd376134b4292d3490b89163b6f7921f
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-09-01 03:49:34 +0300

    Start command

---
+++ CndBot/Core/Commands/StartCommand.cs
@@ -0,0 +1,20 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace CndBot.Core
+{
+    public class StartCommand : BaseCommand
+    {
+        private const string WELCOME_MSG = "Ласкаво просимо до офіційного телеграм-боту Народного Дому " +
+                                           "міста Червоноград. Ви можете переглянути список доступних команд, " +
+                                           "натиснувши відповідну кнопку на панелі, розташованій під цим " +
+                                           "повідомленням.";
+        public override async void ExecuteCommand(ITelegramBotClient botClient, Update update)
+        {
+            if (update.Message != null)
+            {
+                await botClient.SendTextMessageAsync(update.Message.Chat, WELCOME_MSG);
+            }
+        }
+    }
+}
