d361ebeee6dea93fcbdb38d4a837f0a602ba1f0d
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-09-01 03:49:42 +0300

    Get info command

---
+++ CndBot/Core/Commands/GetInfoCommand.cs
@@ -0,0 +1,17 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace CndBot.Core
+{
+    public class GetInfoCommand : BaseCommand
+    {
+        private const string INFO_MSG =    "Це просто тестове повідомлення про інфу";
+        public override async void ExecuteCommand(ITelegramBotClient botClient, Update update)
+        {
+            if (update.Message != null)
+            {
+                await botClient.SendTextMessageAsync(update.Message.Chat, INFO_MSG);
+            }
+        }
+    }
+}
