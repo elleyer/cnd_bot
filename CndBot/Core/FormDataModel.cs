27e36b3d532b1b7f136470c5e7f31941b84cdafc
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-09-05 17:29:40 +0300

    Create FromDataModel

---
+++ CndBot/Core/FormDataModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CndBot.Core.Actions;
+
+namespace CndBot.Core
+{
+    public class FormDataModel
+    {
+        [Key]
+        public virtual int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Description { get; set; }
+        
+        public FormType FormType { get; set; }
+        
+        public long UserId { get; set; }
+        
+        public int Age { get; set; }
+        
+        public bool Approved { get; set; }
+    }
+}
